52a648556a98c0989915d1a87e40cfa7dcfcc2ba
Jean Joseph George <jean.joseph@outlook.com>
2018-11-06 23:50:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  var message = string.toUpperCase();
+  console.log (message);
+}
+
+function logWhisper(string){
+  var message = string.toLowerCase();
+  console.log (message);
+}
+
