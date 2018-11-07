e243df1802299764334ca0e26f49f75519c1b9ef
Jean Joseph George <jean.joseph@outlook.com>
2018-11-07 00:34:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma (string) {
 	if (string === ("I love you, Grandma.")) {
 	return ("I love you, too.");
 	}
-	else if ((string.toUppercase()) === string) {
+	else if ((string.toUpperCase()) === string) {
 	return ("YES INDEED");
 	} else (false); {
 	return ("I can\'t hear you!");
